373eadfc1f31accc8c86c3c7c3c403055d94d90d
ipritish <pritishjain2009@gmail.com>
2015-12-06 22:44:51 +0530

    parser object

--- src/com/parser/jsonobject/JsonFile.java
+++ src/com/parser/jsonobject/JsonFile.java
@@ -1,5 +1,22 @@
 package com.parser.jsonobject;
 
+import java.util.ArrayList;
+
 public class JsonFile {
+	
+	private ArrayList<JsonObject> objectlist;
+
+	public ArrayList<JsonObject> getObjectlist() {
+		return objectlist;
+	}
+
+	public void setObjectlist(ArrayList<JsonObject> objectlist) {
+		this.objectlist = objectlist;
+	}
+	
+	public JsonFile()
+	{
+		setObjectlist(new ArrayList<JsonObject>());
+	}
 
 }
--- src/com/parser/jsonobject/JsonObject.java
+++ src/com/parser/jsonobject/JsonObject.java
@@ -1,5 +1,27 @@
 package com.parser.jsonobject;
 
 public class JsonObject {
+	
+	private JsonKey key;
+	private JsonValue value;
+	
+	public JsonKey getKey() {
+		return key;
+	}
+	public void setKey(JsonKey key) {
+		this.key = key;
+	}
+	public JsonValue getValue() {
+		return value;
+	}
+	public void setValue(JsonValue value) {
+		this.value = value;
+	}
+	
+	public JsonObject()
+	{
+		setKey(new JsonKey());
+		setValue(new JsonValue());
+	}
 
 }
