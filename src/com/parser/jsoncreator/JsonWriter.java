60d13e258b12754e43e23a1ba90560b9aee01ac5
ipritish <pritishjain2009@gmail.com>
2015-12-08 14:05:35 +0530

    json parser

--- src/com/parser/jsoncreator/JsonWriter.java
+++ src/com/parser/jsoncreator/JsonWriter.java
@@ -7,32 +7,41 @@ import com.parser.jsonobject.JsonObject;
 
 public class JsonWriter {
 	
+	private static StringBuilder sb = new StringBuilder();
+	
 	public static void JsonWrite(Json json)
 	{
+		sb.append("{\n");
 		//write all the string first than
 		//iterate through object and separate function object writing
 		//Iterator<String> stringObject = json.getMapString().
 		for (Entry<String, String> entry : json.getMapString().entrySet()) {
-		    System.out.println("Key : " + entry.getKey() + "Value: " + entry.getValue());
+		    sb.append("\t\"" + entry.getKey() + "\""+ "\t:\t\"" + entry.getValue() + "\"\n");
 		    
 		    
 		}
 		
 		for (Entry<String, JsonObject> entry : json.getMapObjects().entrySet()) {
-		    System.out.println("Key : " + entry.getKey() + "Value: ");
+		    //System.out.println("Key : " + entry.getKey() + "Value: ");
+			sb.append("\t\"" + entry.getKey() + "\"" + "\t:\t");
 		    JsonObjectWrite(entry.getValue());
 		    		    
 		}
+		sb.append("}");
+		System.out.println(sb.toString());
 		
 	}
 	
 	public static void JsonObjectWrite(JsonObject jsonObject)
 	{
+		sb.append("{\n");
+		
 		for (Entry<String, String> entry : jsonObject.getMapString().entrySet()) {
-		    System.out.println("Key : " + entry.getKey() + "Value: " + entry.getValue());
-		    
+		    //System.out.println("Key : " + entry.getKey() + "Value: " + entry.getValue());
+			sb.append("\t\"" + entry.getKey() + "\""+ "\t:\t\"" + entry.getValue() + "\"\n");
 		    
 		}
+		sb.append("}\n");
 		
 		for (Entry<String, JsonObject> entry : jsonObject.getMapObject().entrySet()) {
 			JsonObjectWrite(entry.getValue());
