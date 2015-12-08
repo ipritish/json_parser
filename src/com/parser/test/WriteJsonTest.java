d97c23051c895949cf2114d2a2791172db32481c
ipritish <pritishjain2009@gmail.com>
2015-12-08 14:35:57 +0530

    test json parser

--- src/com/parser/test/WriteJsonTest.java
+++ src/com/parser/test/WriteJsonTest.java
@@ -12,6 +12,10 @@ public class WriteJsonTest {
 		json.getMapString().put("first", "trial");
 		json.getMapObjects().put("second", new JsonObject());
 		json.getMapObjects().get("second").getMapString().put("insidefirst", "insidesecond");
+		json.getMapObjects().put("third", new JsonObject());
+		json.getMapObjects().get("third").getMapString().put("insidesecond", "insidesecond");
+		json.getMapObjects().get("third").getMapObject().put("insideinsidefirst", new JsonObject());
+		json.getMapObjects().get("third").getMapObject().get("insideinsidefirst").getMapString().put("test","test");
 		JsonWriter.JsonWrite(json);
 		
 	}
