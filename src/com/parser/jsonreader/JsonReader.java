6cfb7b9ea379f2780edba156c4305c43155a993c
ipritish <pritishjain2009@gmail.com>
2015-12-11 14:57:21 +0530

    reader test

--- src/com/parser/jsonreader/JsonReader.java
+++ src/com/parser/jsonreader/JsonReader.java
@@ -1,5 +1,14 @@
 package com.parser.jsonreader;
 
+import com.parser.jsonobject.Json;
+
 public class JsonReader {
+	
+	public static Json parse(String jsonString)
+	{
+		Json json = new Json();
+		//do manipulation and create proper variable
+		return json;	
+	}
 
 }
--- src/com/parser/test/ReadJsonTest.java
+++ src/com/parser/test/ReadJsonTest.java
@@ -1,5 +1,37 @@
 package com.parser.test;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.parser.jsonobject.Json;
+import com.parser.jsonreader.JsonReader;
+
 public class ReadJsonTest {
+	
+	public static void main(String[] args)
+	{
+		//jsonString
+		Charset charset = Charset.forName("US-ASCII");
+		List<String> allLines = new ArrayList<String>();
+		Path file = FileSystems.getDefault().getPath("user.json");
+		try {
+			allLines = Files.readAllLines(file, charset);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String jsonString = new String("");
+		for (String var : allLines)
+		{
+			jsonString+=var;
+		}
+		Json json = JsonReader.parse(jsonString);
+		System.out.println(json.toString());
+	}
 
 }
