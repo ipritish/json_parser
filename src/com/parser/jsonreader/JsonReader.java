1f3acb676e2fdbdbea2785f0d51ea36a3b7528d6
ipritish <pritishjain2009@gmail.com>
2015-12-12 11:32:44 +0530

    json project

--- src/com/parser/jsonreader/JsonReader.java
+++ src/com/parser/jsonreader/JsonReader.java
@@ -1,11 +1,34 @@
 package com.parser.jsonreader;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.parser.jsonobject.Json;
 
 public class JsonReader {
 	
-	public static Json parse(String jsonString)
+	public static Json parse(String filePath)
 	{
+		//jsonString
+		Charset charset = Charset.forName("US-ASCII");
+		List<String> allLines = new ArrayList<String>();
+		Path file = FileSystems.getDefault().getPath(filePath);
+		try {
+				allLines = Files.readAllLines(file, charset);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String jsonString = new String("");
+		for (String var : allLines)
+		{
+			jsonString+=var;
+		}
+		System.out.println(jsonString);
 		Json json = new Json();
 		//do manipulation and create proper variable
 		return json;	
--- src/com/parser/test/ReadJsonTest.java
+++ src/com/parser/test/ReadJsonTest.java
@@ -1,13 +1,5 @@
 package com.parser.test;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.parser.jsonobject.Json;
 import com.parser.jsonreader.JsonReader;
 
@@ -15,23 +7,8 @@ public class ReadJsonTest {
 	
 	public static void main(String[] args)
 	{
-		//jsonString
-		Charset charset = Charset.forName("US-ASCII");
-		List<String> allLines = new ArrayList<String>();
-		Path file = FileSystems.getDefault().getPath("user.json");
-		try {
-			allLines = Files.readAllLines(file, charset);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String jsonString = new String("");
-		for (String var : allLines)
-		{
-			jsonString+=var;
-		}
-		Json json = JsonReader.parse(jsonString);
-		System.out.println(json.toString());
+		Json json = JsonReader.parse("user.json");
+		//System.out.println(json.toString());
 	}
 
 }
